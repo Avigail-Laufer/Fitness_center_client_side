4bbfbcf54d90a60b83b102aea85c971ef8cdf300
AvigailZirel <al3293374@gmail.com>
2024-05-15 18:43:34 +0300

    home training

---
+++ client/src/components/PersonalArea.jsx
@@ -0,0 +1,34 @@
+import React, { createElement, useEffect, useState } from "react"
+
+export default function PersonalArea() {
+    const [apiRequest, setapiRequest] = useState([]);
+        
+        const onMouseClickShowTrainingClient= async (id) => {
+            try {
+                const url = "http://localhost:5168/api/training/${id}"
+                // Get users list in API request
+                const response = await fetch(url);
+
+                // Convert the response to json
+                const json = await response.json();
+                console.log("data: " , json.items);
+
+                // Save the data
+                setapiRequest(json.items)
+            } catch (error) {
+                console.log("error: ", error);
+            }
+        };
+
+
+
+    
+   
+    return (
+        <>
+            <td onClick={() => onMouseClickShowTrainingClient("203669558 ")}> {}</td>
+        </>
+    )
+
+  
+ }
---
+++ client/src/components/homeTraining.jsx
@@ -0,0 +1,49 @@
+import { click } from "@testing-library/user-event/dist/click"
+import React, { createElement, useEffect, useState } from "react"
+
+
+export default function HomeTraining() {
+    
+
+    const [isTrue, setIstrue] = useState(false)
+    const [response, setResponse] = useState(null)
+    const [hover, setHover] = useState(false);
+
+    const onMouseOverCaptureHandler = (item) => {
+        setHover(item.purposeOfTraining);
+    };
+    
+    useEffect( () => {
+        const fetchData = async () => {
+            const url = "http://localhost:5168/api/training"
+            const response = await fetch(url)
+            const responseJson=await (response.json())
+            setResponse(responseJson)
+        
+        
+        }
+
+        fetchData()
+    }, [])
+    return (
+        <>
+            {response ? response.map((item, index) => (
+               
+                <table className='table table-dark'>
+                    <tbody>
+                        <tr>
+
+                            <td onMouseOver={() => onMouseOverCaptureHandler(item)}> {item.name}</td>
+                           
+                            
+                        </tr>
+                    </tbody>
+                </table>
+                
+            )) : <h1>no data received</h1>}
+             <h1 id="purposeOfTraining">{hover}</h1>
+        </>
+    )
+
+  
+ }
