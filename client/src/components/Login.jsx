e13b9a1fd25d96780e8a5611d2c30466818fa3d2
batsiGottliv <batsi1606@gmail.com>
2024-05-21 23:49:25 +0300

    enter - mouse

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,46 +1,35 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
- // from inside the function component
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Login = (props) => {
-  const [password, setPassword] = useState('')
-  const [passwordError, setPasswordError] = useState('')
-  const navigate = useNavigate(); 
+  const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
 
+  const onMouseClick = async () => {
+    try {
+      const url = `http://localhost:5168/api/client/id/${password}`;
+      const response = await fetch(url);
 
-
-
-  
-    const onMouseClick= async () => {
-     
-      try {
-        debugger
-          const url = "http://localhost:5168/api/client/id/"+password;
-         
-          const response = await fetch(url);
-          
-          if (response){
-          navigate(`/PersonalArea/${password}`)
-          }
-          // else{
-          //   navigate(`/ContactUs/${password}`)
-          // }
-          
-
-         
-          // console.log("data: " , json.items);
-      
-      
-
-
-          console.log("data: " , response);
-      } catch (error) {
-          console.log("error: ", error);
+      if (response.ok) {
+        navigate(`/PersonalArea/${password}`);
+      } else {
+        // Handle error or navigate to a different page
+        // navigate(`/ContactUs/${password}`);
+        console.log('Error occurred');
       }
+
+      console.log("Response data:", response);
+    } catch (error) {
+      console.log("Error:", error);
+    }
   };
 
-    
-
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      onMouseClick();
+    }
+  };
 
   return (
     <div className={'mainContainer'}>
@@ -54,16 +43,16 @@ const Login = (props) => {
           placeholder="Enter your password here"
           onChange={(ev) => setPassword(ev.target.value)}
           className={'inputBox'}
+          onKeyUp={handleKeyPress}
         />
         <label className="errorLabel">{passwordError}</label>
       </div>
       <br />
       <div className={'inputContainer'}>
-        <input className={'inputButton'} type="button"  value={'Log in'} onClick={onMouseClick} />
+        <input className={'inputButton'} type="button" value={'Log in'} onClick={onMouseClick} />
       </div>
-      
     </div>
+  );
+};
 
-  )
-  }
-export default Login
+export default Login;
